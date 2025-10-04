5346438b7b49f313f40e5ac9e7efe8a503d2149a
Simon Sotak Gregor <4610874+the21st@users.noreply.github.com>
2025-10-04 17:23:35 +0200

    Update version and release

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 console.log("Rohlik Enhanced loaded");
 
-const VERSION = "v6";
+const VERSION = "v7";
 const dbName = `nutriScoreDB_${VERSION}`;
 const storeName = `nutriScores_${VERSION}`;
 let db;
